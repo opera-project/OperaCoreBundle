d7424860dee3f249a9494a3cea3725e1ed6dbab0
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-07-20 18:20:01 +0200

    Namespaces

--- Resources/skeleton/block.tpl.php
+++ Resources/skeleton/block.tpl.php
@@ -2,6 +2,9 @@
 
 namespace <?= $namespace ?>;
 
+use Opera\CoreBundle\BlockType\BlockTypeInterface;
+use Opera\CoreBundle\BlockType\BaseBlock;
+
 class <?= $class_name ?> extends BaseBlock implements BlockTypeInterface
 {
     public function getType() : string
