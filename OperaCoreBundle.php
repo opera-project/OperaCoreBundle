8a740c33ddf7ce9494824a14e511e5d179db0ef2
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-07-20 11:45:01 +0200

    Register BlockTypePass

--- OperaCoreBundle.php
+++ OperaCoreBundle.php
@@ -3,7 +3,13 @@
 namespace Opera\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Opera\CoreBundle\DependencyInjection\Compiler\BlockTypePass;
 
 class OperaCoreBundle extends Bundle
 {
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->addCompilerPass(new BlockTypePass());
+    }
 }
