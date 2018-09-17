1a2c4d254e7a84a633e901a724579bd31127d46a
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-09-17 22:18:03 +0200

    Autoconfigure

--- OperaCoreBundle.php
+++ OperaCoreBundle.php
@@ -6,13 +6,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Opera\CoreBundle\DependencyInjection\Compiler\BlockTypePass;
 use Opera\CoreBundle\DependencyInjection\Compiler\ResourceCompilerPass;
+use Opera\CoreBundle\BlockType\BlockTypeInterface;
 
 class OperaCoreBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
+        $container->registerForAutoconfiguration(BlockTypeInterface::class)->addTag('cms.block_type');
         $container->addCompilerPass(new BlockTypePass());
         $container->addCompilerPass(new ResourceCompilerPass());
-
     }
 }
