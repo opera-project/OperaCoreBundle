5eda691d16c689f18da1a030864f5ab8b7d18e2a
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-09-23 15:32:54 +0200

    Better maker

--- Resources/skeleton/block.tpl.php
+++ Resources/skeleton/block.tpl.php
@@ -5,9 +5,16 @@ namespace <?= $namespace ?>;
 use Opera\CoreBundle\BlockType\BlockTypeInterface;
 use Opera\CoreBundle\BlockType\BaseBlock;
 use Opera\CoreBundle\Entity\Block;
+use Symfony\Bundle\FrameworkBundle\Controller\ControllerTrait;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 
-class <?= $class_name ?> extends BaseBlock implements BlockTypeInterface
+
+class <?= $class_name ?> extends BaseBlock implements BlockTypeInterface, ContainerAwareInterface
 {
+    use ControllerTrait;
+    use ContainerAwareTrait;
+
     public function getType() : string
     {
         return '<?= $block ?>';
@@ -15,6 +22,9 @@ class <?= $class_name ?> extends BaseBlock implements BlockTypeInterface
 
     public function execute(Block $block) : array
     {    
+        // Do here your controller
+        // eg $form = $this->createForm(MyType::class);
+
         return [
             // Set here your template vars
         ];
