a6cde92fba1f2dc138e4d97ce0c56bce55856911
Cedric Lombardot <cedric.lombardot@gmail.com>
2018-09-23 15:31:21 +0200

    Better maker

--- Resources/skeleton/block.tpl.php
+++ Resources/skeleton/block.tpl.php
@@ -4,11 +4,19 @@ namespace <?= $namespace ?>;
 
 use Opera\CoreBundle\BlockType\BlockTypeInterface;
 use Opera\CoreBundle\BlockType\BaseBlock;
+use Opera\CoreBundle\Entity\Block;
 
 class <?= $class_name ?> extends BaseBlock implements BlockTypeInterface
 {
     public function getType() : string
     {
         return '<?= $block ?>';
+    }
+
+    public function execute(Block $block) : array
+    {    
+        return [
+            // Set here your template vars
+        ];
     }
 }
